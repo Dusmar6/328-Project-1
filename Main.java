5017835a04f5746ca4299425ab21163be0020852
Dustin Martin <36971079+Dusmar6@users.noreply.github.com>
2019-11-26 18:28:51 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
+// Greyson Cabrera 014121118
+// Dustin Martin 015180085
+
+
 import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -10,27 +15,84 @@ public class Main {
         int[] inputArr = new int[10];
         String input = "";
 
+        String algos = "";
+
         try (Scanner in = new Scanner(System.in)) {
 
             while (!input.equals("exit")) {
 
-                System.out.println("\nEnter a valid option\n1: User generated array\n2: Randomly generated array\nexit: end the program\n");
+                System.out.println("\nEnter a valid option\n1: User generated array\n2: Randomly generated array\n3: Estimate runtime \n4: Exit\n");
                 input = in.nextLine();
 
-                int algor1Sum = 0;
+                
 
                 switch (input) {
 
                     case "1":
                         inputArr = userEnteredArray(in);
-                        testAlgorithms(inputArr);
+                        algos = "1234";
+                        testAlgorithms(inputArr, algos);
                         break;
                     case "2":
                         inputArr = randomGenArray(in);
-                        testAlgorithms(inputArr);
+                        System.out.println("\nEnter a 4-character string denoting which algorithms you'd like to run.");
+                        algos = in.nextLine();
+                        testAlgorithms(inputArr, algos);
                         break;
-                    case "exit":
+                    case "3":
+                        System.out.println("\nPick an Algorithm\n1: Solution 1\n2: Solution 2\n3: Solution 3 \n4: Solution 4\n");
+                        String algo_input = in.nextLine();
+
+                        System.out.println("\nEnter array size to get runtime (m):\n");
+                        int m = Integer.parseInt(in.nextLine());
+
+                        System.out.println("\nEnter array size to predict runtime (n):\n");
+                        int n = Integer.parseInt(in.nextLine());
+
+                        inputArr = random_gen_array(m);
+                        int[] new_arr = random_gen_array(n);
+
+                        double milli_time_m =-1;
+                        double milli_time_n =-1;
+                        double predicted_time_n =-1;
+
+                        System.out.println("\n-----------------------------");
+                        System.out.println("Runtime for M: ");
+                        switch(algo_input){
+
+                          case "1":
+                            algos = "1";
+                            milli_time_m = option_3(inputArr, algos);
+                            break;
+                          case "2":
+                            algos = "2";
+                            milli_time_m = option_3(inputArr, algos);
+                            break;
+
+                          case "3":
+                            algos = "3";
+                            milli_time_m = option_3(inputArr, algos);
+                            break;
+
+                          case "4":
+                            algos = "4";
+                            milli_time_m = option_3(inputArr, algos);
+                            break;
+
+                        }
+                        System.out.println("-----------------------------");
+
+                        System.out.println("\n-----------------------------");
+                        predicted_time_n = (milli_time_m * n)/m;
+                        System.out.println("Predicted time for size n: "+ predicted_time_n);
+                        System.out.println("\nActual runtime for size n: ");
+                        milli_time_n = option_3(new_arr, algos);
+                        System.out.println("-----------------------------");
+
+                        break;
+                    case "4":
                         System.out.println("Goodbye!");
+                        System.exit(0);
                         break;
                     default:
                         System.out.println("Not a valid input.");
@@ -38,14 +100,11 @@ public class Main {
 
                 }
 
-
-
             }
 
         } catch (Exception e) {
             System.out.println("Improper Input.");
         }
-
     }
 
     public static int solutionOne(int[] array) {
@@ -66,10 +125,11 @@ public class Main {
 
 
     public static int solutionTwo( int[] a){
+      int n = a.length;
       int max_sum = 0;
-      for (i=0; i<n; i++){
+      for (int i=0; i<n; i++){
         int this_sum = 0;
-        for (j=i; j<n; j++){
+        for (int j=i; j<n; j++){
           this_sum += a[j];
           if (this_sum>max_sum){
             max_sum = this_sum;
@@ -136,21 +196,129 @@ public class Main {
         return maxLeftSum + maxRightSum;
 
     }
+    
+    public static int solutionFour(int[] a){
+      
+      int max_sum = 0;
+      int this_sum = 0;
 
-    public static void testAlgorithms(int[] inputArr) {
+      for (int i =0; i<a.length; i++){
+        this_sum = this_sum + a[i];
+        if(this_sum > max_sum){
+          max_sum = this_sum;
+        }
+        else if(this_sum<0){
+          this_sum =0;
+        }
+      }
+      return max_sum;
+    }
+    public static double print_runtime(long nano){
+      double d_nano = nano;
+      double milli = d_nano/1000000;
+      double seconds = milli/1000;
 
-        long startTime = System.nanoTime();
-        int algor1Sum = solutionOne(inputArr);
-        long endTime = System.nanoTime();
-        System.out.println("System run time, " + (endTime - startTime) + " nano seconds.");
-        System.out.println("First algorithms max sum: " + algor1Sum);
+      if(milli >1000){
+        System.out.println("Runtime: "+ seconds + " seconds");
+      }
+      else{
+        System.out.println("Runtime: "+ milli +" milliseconds");
+      }
+      return milli;
+    }
 
-        startTime = System.nanoTime();
-        int algor3Sum = mssJunior(inputArr, 0, inputArr.length-1);
-        endTime = System.nanoTime();
-        System.out.println("System run time, " + (endTime - startTime) + " nano seconds.");
-        System.out.println("Third algorithms max sum: " + algor3Sum);
+    public static void testAlgorithms(int[] inputArr, String algos) {
+        long startTime;
+        long endTime;
+        long nano_runtime;
+        System.out.println("\n-----------------------------");
+        if (algos.contains("1")){
 
+          startTime = System.nanoTime();
+          int algor1Sum = solutionOne(inputArr);
+          endTime = System.nanoTime();
+          nano_runtime = (endTime - startTime);
+          System.out.println("First algorithms max sum: " + algor1Sum);
+          print_runtime(nano_runtime);
+        }
+        
+        if (algos.contains("2")){
+          startTime = System.nanoTime();
+          int algor2Sum = solutionTwo(inputArr);
+          endTime = System.nanoTime();
+
+          nano_runtime = (endTime - startTime);   
+          System.out.println("Second algorithms max sum: " + algor2Sum);
+          print_runtime(nano_runtime);
+        }
+
+        if (algos.contains("3")){
+          startTime = System.nanoTime();
+          int algor3Sum = mssJunior(inputArr, 0, inputArr.length-1);
+          endTime = System.nanoTime();
+          nano_runtime = (endTime - startTime);
+          System.out.println("Third algorithms max sum: " + algor3Sum);
+          print_runtime(nano_runtime);
+        }
+
+        if (algos.contains("4")){
+          startTime = System.nanoTime();
+          int algor4Sum = solutionFour(inputArr);
+          endTime = System.nanoTime();
+          nano_runtime = (endTime - startTime);
+          System.out.println("Fourth algorithms max sum: " + algor4Sum);
+          print_runtime(nano_runtime);
+          
+        }
+        System.out.println("\n-----------------------------");
+    }
+
+    public static double option_3(int[] inputArr, String algos) {
+        long startTime;
+        long endTime;
+        long nano_runtime;
+        
+        
+        if (algos.contains("1")){
+
+          startTime = System.nanoTime();
+          int algor1Sum = solutionOne(inputArr);
+          endTime = System.nanoTime();
+          nano_runtime = (endTime - startTime);
+          System.out.println("First algorithms max sum: " + algor1Sum);
+          return print_runtime(nano_runtime);
+        }
+        
+        else if (algos.contains("2")){
+          startTime = System.nanoTime();
+          int algor2Sum = solutionTwo(inputArr);
+          endTime = System.nanoTime();
+
+          nano_runtime = (endTime - startTime);   
+          System.out.println("Second algorithms max sum: " + algor2Sum);
+          return print_runtime(nano_runtime);
+        }
+
+        else if (algos.contains("3")){
+          startTime = System.nanoTime();
+          int algor3Sum = mssJunior(inputArr, 0, inputArr.length-1);
+          endTime = System.nanoTime();
+          nano_runtime = (endTime - startTime);
+          System.out.println("Third algorithms max sum: " + algor3Sum);
+          return print_runtime(nano_runtime);
+        }
+
+        else if (algos.contains("4")){
+          startTime = System.nanoTime();
+          int algor4Sum = solutionFour(inputArr);
+          endTime = System.nanoTime();
+          nano_runtime = (endTime - startTime);
+          System.out.println("Fourth algorithms max sum: " + algor4Sum);
+          return print_runtime(nano_runtime);
+          
+        }
+        return 0.0;
+        
     }
 
     /**
@@ -195,6 +363,21 @@ public class Main {
         System.out.print("Enter the length of an array to generate: ");
         int[] intArray = new int[in.nextInt()];
         in.nextLine();
+
+        //generate random values for the array
+        for (int i = 0; i < intArray.length; i++) {
+
+            intArray[i] = rand.nextInt(101) - 50;
+
+        }
+
+        return intArray;
+
+    }
+    public static int[] random_gen_array(int n) {
+        Random rand = new Random();
+        
+        int[] intArray = new int[n];
 
         //generate random values for the array
         for (int i = 0; i < intArray.length; i++) {
